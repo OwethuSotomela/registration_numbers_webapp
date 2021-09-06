edd07e5889e5b696ff5d683ba929c94b5f69ae72
OwethuSotomela <owethusotomela@gmail.com>
2021-09-06 19:11:33 +0200

    unit tests

--- test/registration_test.js
+++ test/registration_test.js
@@ -16,50 +16,27 @@ const registration = Registration(pool);
 describe('List of registration numbers', async function () {
     it('Should display the list of all entered registration numbers', async function () {
 
-        await registration.insertReg('CA 123489');
-        await registration.insertReg('CL 890-765');
-        await registration.insertReg('CW 667-987');
-        await registration.insertReg('CL 254 369');
-        await registration.insertReg('CA 890-098');
+        await registration.emptyDB()
+        var all = ["CA 456-456", "CA 123-123", "CW 456-456", "CL 456-456", "CA 589-365", "CL 589-357", "CW 254-365"]
 
-        // var regNumber1 = await registration.getRegNumbers('CA 123489');
-        // var regNumber2 = await registration.getRegNumbers('CL 890-765');
-        // var regNumber3 = await registration.getRegNumbers('CW 667-987');
-        // var regNumber4 = await registration.getRegNumbers('CL 254 369');
-        // var regNumber5 = await registration.getRegNumbers('CA 890-098');
+        var regList = [];
 
-        // assert.equal('CA 123489', regNumber1);
-        // assert.equal('CL 890-765', regNumber2);
-        // assert.equal('CW 667-987', regNumber3);
-        // assert.equal('CL 254 369', regNumber4);
-        // assert.equal('CA 890-098', regNumber5);
+        await registration.insertReg("CA 456-456")
+        await registration.insertReg("CA 123-123")
+        await registration.insertReg("CW 456-456")
+        await registration.insertReg("CL 456-456")
+        await registration.insertReg("CA 589-365")
+        await registration.insertReg("CL 589-357")
+        await registration.insertReg("CW 254-365")
 
-        assert.equal('CA 123489', getRegNumbers())
-        assert.equal('CL 890-765', getRegNumbers())
-        assert.equal('CW 667-987', getRegNumbers())
-        assert.equal('CL 254 369', getRegNumbers())
-        assert.equal('CA 890-098', getRegNumbers())
-    })
-})
+        var getRegNumbs = await registration.getRegNumbers()
 
+        getRegNumbs.forEach(element => {
+            regList.push(element.regnumber)
+        });
 
-describe('Deleting Database', async function () {
-    it('should delete from registration_App database', async function () {
-        await registration.emptyDB();
-        assert.equal(0, await registration.prepopulate())
-    })
-})
+        assert.equal(all.length, regList.length)
+        assert.deepEqual(all, regList)
 
-describe("Returning error messages", async function () {
-
-    it('Should return the message "Registration number already entered"', async function () {
-        let registration = Registration([])
-        var regNumber = "CA 465-857"
-
-        await registration.insertReg(regNumber)
-        await registration.insertReg(regNumber)
-
-        assert.equal('CA 465-857', await registration.getRegNumbers(regNumber))
-        assert.equal('Registration number already added!', await registration.getMessage())
     })
 })
