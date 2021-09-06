f6d83bfcb66e80efdfcdb1a4e2581a6c60321dae
OwethuSotomela <owethusotomela@gmail.com>
2021-09-06 19:57:07 +0200

    unit tests

--- test/registration_test.js
+++ test/registration_test.js
@@ -43,7 +43,7 @@ describe('List of registration numbers', async function () {
 
 describe("Inserts registration numbers to the database", async function () {
     it("Should insert registration numbers to the database", async function () {
-       
+
         await registration.emptyDB();
 
         var all = ["CA 456-456", "CA 123-123", "CW 456-456", "CL 456-456", "CA 589-365", "CL 589-357", "CW 254-365"]
@@ -65,6 +65,49 @@ describe("Inserts registration numbers to the database", async function () {
         });
 
         assert.equal(all.length, regList.length)
+
+        await registration.emptyDB();
     })
 })
+
+describe("Filtering registration numbers", async function () {
+
+    it("Should return registration numbers for Cape Town", async function () {
+
+        await registration.insertReg("CA 456-456")
+        await registration.insertReg("CA 123-123")
+        await registration.insertReg("CW 456-456")
+        await registration.insertReg("CL 456-456")
+        await registration.insertReg("CA 589-365")
+        await registration.insertReg("CL 589-357")
+        await registration.insertReg("CW 254-365")
+
+        var capeTown = ["CA 456-456", "CA 123-123", "CA 589-365"]
+
+        var regListCT = [];
+
+        var getRegNumberFilter = await registration.regPlate(1)
+        getRegNumberFilter.forEach(element => {
+            regListCT.push(element.regnumber)
+        })
+
+        assert.deepEqual(capeTown, regListCT);
+    }) 
+
+    it("Should return registration numbers for Stellenbotch", async function(){
+
+        var stellenbotch = ["CL 456-456", "CL 589-357"]
+
+        var regListStellenbotch = []
+
+        var getRegNumberFilter = await registration.regPlate(2)
+        getRegNumberFilter.forEach(element => {
+            regListStellenbotch.push(element.regnumber)
+        })
+
+        assert.deepEqual(stellenbotch, regListStellenbotch);
+    })
+})
+
+
 
